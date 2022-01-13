b1c8cc863d68b3553d72029ffd2377bee1a38316
Amutha varsni R <amuthavarsni.cs18@bitsathy.ac.in>
2022-01-13 11:41:18 +0530

    Lamp stack created

--- cookbooks/webserver/metadata.rb
+++ cookbooks/webserver/metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures webserver'
 version '0.1.0'
 chef_version '>= 16.0'
-
+depends 'mysql'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -3,3 +3,38 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+mysql_service 'default' do
+  port '3306'
+  version '8.0'
+  initial_root_password 'password'
+  action [:create, :start]
+end
+
+mysql_database 'todo' do
+  user 'root'
+  action :create
+end
+
+mysql_user 'admin' do
+  password 'password'
+  action :create
+end
+
+mysql_user 'admin' do
+  password 'password'
+  database_name 'todo'
+  host '%'
+  privileges [:select,:update,:insert]
+  action :grant
+end
+
+package 'httpd'
+service 'httpd' do
+action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+source 'index.html.erb'
+end
+
